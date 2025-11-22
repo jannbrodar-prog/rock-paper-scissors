f4b7a2868be3450ff358cc3438477ef3dc61b03d
Jan Brodar <241314424+jannbrodar-prog@users.noreply.github.com>
2025-11-22 22:00:25 +0100

    Finish game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -4,11 +4,6 @@
 
 console.log("Hello!");
 
-//here we declare 2 variables to keep the score
-
-let humanScore = 0;
-let computerScore = 0;
-
 // lets make a function that returns a random string (rock, paper or scissors)
 // PSEUDOCODE
 // get a random number between 0 and 1 and store it in a variable
@@ -51,27 +46,101 @@ function getHumanChoice() {
 // representing the round winner, such as: “You lose! Paper beats Rock”
 // Increment the humanScore or computerScore variable based on the round winner.
 
-function playRound(humanChoice, computerChoice) {
+// function playRound(humanChoice, computerChoice) {
+//   if (humanChoice == computerChoice) {
+//     return "Its a tie!";
+//   } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+//     return "You lose, Paper beats Rock!";
+//   } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+//     return "You win, Rock beats Scissors!";
+//   } else if (humanChoice == "Paper" && computerChoice == "Rock") {
+//     return "You win, Paper beats Rock!";
+//   } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+//     return "You lose, Scissors beats Paper!";
+//   } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+//     return "You lose, Rock beats Scissors!";
+//   } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+//     return "You win, Scissors beats Paper!";
+//   } else {
+//     return "You entered an invalid word.";
+//   }
+// }
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// console.log(playRound(humanSelection, computerSelection));
+
+// Write the logic to play the entire game
+//PSEUDOCODE
+// Declare function playGame
+// Move your playRound function and score variables
+// so that they’re declared inside of the new playGame function
+// Play 5 rounds by calling playRound 5 times
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
     return "Its a tie!";
   } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+    computerScore++;
     return "You lose, Paper beats Rock!";
   } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+    humanScore++;
     return "You win, Rock beats Scissors!";
   } else if (humanChoice == "Paper" && computerChoice == "Rock") {
+    humanScore++;
     return "You win, Paper beats Rock!";
   } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+    computerScore++;
     return "You lose, Scissors beats Paper!";
   } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+    computerScore++;
     return "You lose, Rock beats Scissors!";
   } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+    humanScore++;
     return "You win, Scissors beats Paper!";
   } else {
     return "You entered an invalid word.";
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+let humanSelection2 = getHumanChoice();
+let computerSelection2 = getComputerChoice();
+
+let humanSelection3 = getHumanChoice();
+let computerSelection3 = getComputerChoice();
+
+let humanSelection4 = getHumanChoice();
+let computerSelection4 = getComputerChoice();
+
+let humanSelection5 = getHumanChoice();
+let computerSelection5 = getComputerChoice();
 
 console.log(playRound(humanSelection, computerSelection));
+console.log("Computer score is: " + computerScore);
+console.log("Your score is: " + humanScore);
+
+console.log(playRound(humanSelection2, computerSelection2));
+console.log("Computer score is: " + computerScore);
+console.log("Your score is: " + humanScore);
+
+console.log(playRound(humanSelection3, computerSelection3));
+console.log("Computer score is: " + computerScore);
+console.log("Your score is: " + humanScore);
+
+console.log(playRound(humanSelection4, computerSelection4));
+console.log("Computer score is: " + computerScore);
+console.log("Your score is: " + humanScore);
+
+console.log(playRound(humanSelection5, computerSelection5));
+console.log("Computer score is: " + computerScore);
+console.log("Your score is: " + humanScore);
+
+}
+playGame();
+
